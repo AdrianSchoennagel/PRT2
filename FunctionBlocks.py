206a7b0f119057a7d79bf492c4f5990489c303f6
Adrian Schönnagel <a.schoennagel@web.de>
2020-06-26 10:12:08 +0200

    new infrastructure using commun ode

--- FunctionBlocks.py
+++ FunctionBlocks.py
@@ -4,84 +4,97 @@ from numpy import cos, sin, tan
 import scipy.integrate as sci
 
 from Parameters import *
+from input import *
 
-class Abstract_Block(ABC):
-    def __init__(self, state_sv):
+class Solver:
+    def __init__(self, tt):
+        self.tt = tt
+        self.state_start = [0, 0, 0, 0, 0]
+        self.pt1 = PT1_Block(para_pt1.Kp, para_pt1.T)
+        self.dt1 = DT1_Block(1, 1)
+        self.pt2 = PT2_Block(1, 1, 1)
+        self.pi  = PI_Block(1, 2)
+
+    def ode(self, t, x, returnDxDt = True):
+        u = cubic_jump(t)
+        dxdt = np.concatenate((self.pt1.ode(x[0], u), self.dt1.ode(x[1], u), self.pt2.ode(x[2:4], u), self.pi.ode(x[4], u)))
+        y    = np.concatenate((self.pt1.out(x[0], u), self.dt1.out(x[1], u), self.pt2.out(x[2:4], u), self.pi.out(x[4], u)))
+        if returnDxDt:
+            return dxdt
+        else:
+            return y
+
+    def calc(self):
+        solv = sci.solve_ivp(self.ode, (sim_para.t0, sim_para.tf), self.state_start, max_step=sim_para.max_step, t_eval=self.tt)
+        ys = None
+        for i in range(0, len(self.tt)):
+            y = self.ode(self.tt[i], solv.y.T[i], False)
+            if ys is None:
+                ys = [y]
+            else:
+                ys = np.append(ys, [y], axis=0)
+        return ys.T
+
+
+
+class PT1_Block():
+    def __init__(self, Kp, T, state_sv = 0):
         self.state_sv = state_sv
-
-    @abstractmethod
-    def ode(self, t, x, u):
-        pass
-
-    def calc(self, u, t0, t1):
-        solv = sci.solve_ivp(self.ode, (t0, t1), self.state_sv, args=(u,), max_step=sim_para.max_step)
-        self.state_sv = solv.y.T[1]
-        return solv.y.T[0]
-
-class D_Block():
-    def __init__(self, Kd, u_init = None):
-        self.Kd = Kd
-        self.u_past = u_init
-
-    def calc(self, u):
-        if self.u_past is None:
-            self.u_past = u
-        du = np.gradient([self.u_past, u], sim_para.dt)
-        self.u_past = u
-        return du * self.Kd
-
-
-class PT1_Block(Abstract_Block):
-    def __init__(self, Kp, T, state_sv):
-        super().__init__(state_sv)
         self.Kp = Kp
         self.T = T
 
-    def ode(self, t, x, u):
+    def ode(self, x, u):
         dxdt = np.array([(self.Kp * u - x) / self.T])
         return dxdt
 
+    def out(self, x, u):
+        return np.array([x])
+
 class DT1_Block():
-    def __init__(self, Kp, T1, state_sv = 0):
+    def __init__(self, Kp, T1):
         self.Kp = Kp
         self.T1 = T1
-        self.pt1 = PT1_Block(Kp, T1, state_sv)
-        self.d = D_Block(1)
+        self.pt1 = PT1_Block(Kp, T1)
 
-    def calc(self, u, t0, t1):
-        y_pt1 = self.pt1.calc(u, t0, t1)
-        y_out = self.d.calc(y_pt1[0])
-        return y_out
+    def ode(self, x, u):
+        return self.pt1.ode(x, u)
+
+    def out(self, x, u):
+        return self.pt1.ode(x, u)
 
 class PT2_Block():
-    def __init__(self, Kp, T1, T2, state_sv = 0):
+    def __init__(self, Kp, T1, T2):
         self.Kp = Kp
         self.T1 = T1
         self.T2 = T2
-        self.pt1_1 = PT1_Block(Kp, T1, state_sv)
-        self.pt1_2 = PT1_Block(1, T2, state_sv)
 
-    def calc(self, u, t0, t1):
-        y_pt1 = self.pt1_1.calc(u, t0, t1)
-        y_out = self.pt1_2.calc(y_pt1[0], t0, t1)
-        return y_out
+    def ode(self, x, u):
+        dxdt0 = x[1]
+        dxdt1 = -x[0]/(self.T1 * self.T2) - x[1]*(self.T1 + self.T2)/(self.T1 * self.T2) + u*self.Kp/(self.T1*self.T2)
+        return np.array([dxdt0, dxdt1])
 
-class I_Block(Abstract_Block):
-    def __init__(self, Ki, state_sv):
-        super().__init__(state_sv)
+    def out(self, x, u):
+        return np.array([x[0]])
+
+class I_Block():
+    def __init__(self, Ki):
         self.Ki = Ki
 
-    def ode(self, t, x, u):
+    def ode(self, x, u):
         dxdt = np.array([self.Ki*u])
         return dxdt
 
-class PI_Block:
-    def __init__(self, Ki, Kp, state_sv = 0):
+    def out(self, x, u):
+        return x
+
+class PI_Block():
+    def __init__(self, Ki, Kp):
         self.Ki = Ki
         self.Kp = Kp
-        self.i = I_Block(Ki, state_sv)
 
-    def calc(self, u, t0, t1):
-        y_i = self.i.calc(u, t0, t1)
-        y_out = y_i + self.Kp*u
-        return y_out
+    def ode(self, x, u):
+        dxdt = np.array([self.Ki*u])
+        return dxdt
+
+    def out(self, x, u):
+        return np.array([x + self.Kp*u])
--- main.py
+++ main.py
@@ -6,57 +6,42 @@ from Parameters import *
 import FunctionBlocks as FB
 from input import *
 
-tt = np.arange(sim_para.t0, sim_para.tf + sim_para.dt, sim_para.dt)
-
 x0 = [0]
 
-fig1, (ax1, ax2, ax3, ax4) = plt.subplots(4)
+fig1, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5)
 
-pt1 = FB.PT1_Block(para_pt1.Kp, para_pt1.T, x0)
-d   = FB.D_Block(para_d.Kd)
-dt1 = FB.DT1_Block(1, 1, x0)
-pt2 = FB.PT2_Block(1, 1, 1, x0)
-pi  = FB.PI_Block(1, 2, x0)
+tt = np.arange(sim_para.t0, sim_para.tf + sim_para.dt, sim_para.dt)
+solver = FB.Solver(tt)
+ys = solver.calc()
+y_pt1 = ys[0]
+y_dt1 = ys[1]
+y_pt2 = ys[2]
+y_pi  = ys[3]
 
-x_traj = []
-x_traj2 = []
-x_traj3 = []
-y_out  = []
-u_traj = []
-for t in tt:
-    u_cur = cubic_jump(t)
-    u_traj.append(u_cur)
-
-    y_dt1 = dt1.calc(u_cur, t, t + sim_para.dt)
-    y_pt2 = pt2.calc(u_cur, t, t + sim_para.dt)
-    y_pi  = pi.calc(u_cur, t, t + sim_para.dt)
-
-    x_traj.append(y_dt1[0])
-    x_traj2.append(y_pt2[0])
-    x_traj3.append(y_pi[0])
-
-ax1.plot(tt, u_traj, label='$u(t)$', lw=1, color='b')
-ax2.plot(tt, x_traj, label='$y_dt1(t)$', lw=1, color='r')
-ax3.plot(tt, x_traj2, label='$y_pt2(t)$', lw=1, color='r')
-ax4.plot(tt, x_traj3, label='$y_pi(t)$', lw=1, color='r')
+ax1.plot(tt, np.vectorize(cubic_jump)(tt), label='$u(t)$', lw=1, color='b')
+ax2.plot(tt, y_pt1, label='$y_pt1(t)$', lw=1, color='r')
+ax3.plot(tt, y_dt1, label='$y_dt1(t)$', lw=1, color='r')
+ax4.plot(tt, y_pt2, label='$y_pt2(t)$', lw=1, color='r')
+ax5.plot(tt, y_pi, label='$y_pi(t)$', lw=1, color='r')
 
 ax1.set_title('Führungsverlauf')
 ax1.set_ylabel(r'u(t)')
 ax1.set_xlabel(r't in s')
-ax2.set_title('Ausgang DT1')
-ax2.set_ylabel(r'y_1(t)')
+ax2.set_title('Ausgang PT1')
 ax2.set_xlabel(r't in s')
-ax3.set_title('Ausgang PT2')
-ax3.set_ylabel(r'y_1(t)')
+ax3.set_title('Ausgang DT1')
 ax3.set_xlabel(r't in s')
-ax4.set_title('Ausgang PI')
-ax4.set_ylabel(r'y_1(t)')
+ax4.set_title('Ausgang PT2')
 ax4.set_xlabel(r't in s')
+ax5.set_title('Ausgang PI')
+ax5.set_xlabel(r't in s')
 
 ax1.grid(True)
 ax2.grid(True)
 ax3.grid(True)
 ax4.grid(True)
+ax5.grid(True)
+
 
 plt.tight_layout()
 plt.show()
